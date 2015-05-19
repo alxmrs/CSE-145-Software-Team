11db98d752e2c56f8b34563d538565bba9f2983b
arosengarten <alexrosengarten@gmail.com>
2015-05-19 11:12:01 -0700

    Created simple data storage API

--- source/Databse/api.py
+++ source/Databse/api.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 '''
 CSE 145 EEG Data Storage API
 
@@ -21,13 +23,15 @@ Table Fields:
 '''
 
 import sqlite3 as lite
+import datetime as dt
+import json
 import sys
 
 
 class DataStorage:
 
     #initOrLoadDatabase
-    def __init__(self, dbname='eeg_data.db'):
+    def __init__(self, dbname='eeg_data', tablename='EEG_data'):
         #set target database
         if(('.db' in dbname) == False):
             self.db_name = dbname + '.db'
@@ -35,31 +39,62 @@ class DataStorage:
             self.db_name = dbname
 
         self.conn = None
+        self.table_name = tablename
 
-        try:
-            # Connect to databse
-            self.conn = lite.connect(self.db_name)
+        self.conn = lite.connect(self.db_name)
 
-            # Check SQLite verion
+        with self.conn:
             self.cur = self.conn.cursor()
-            self.cur.execute('SELECT SQLITE_VERSION()')
-            data = self.cur.fetchone()
-            print "SQLite version: %s" % data
 
-        except lite.Error, e:
-            # print error to console, exit system
-            print "Error %s: " % e.args[0]
-            sys.exit(1)
+            self.cur.executescript("""
+                DROP TABLE IF EXISTS EEG_data;
+                CREATE TABLE EEG_data(
+                  id INTEGER PRIMARY KEY,
+                  group_label VARCHAR(20),
+                  subj_name VARCHAR(20),
+                  date DATETIME,
+                  data BLOB(1000000000)
+                ); """)
 
-        finally:
-            #safely close connection
-            if self.conn:
-                self.conn.close()
+            self.bootstrapData()
+            # Print database version
+            # self.cur.execute('SELECT SQLITE_VERSION()')
+            # data = self.cur.fetchone()
+            # print "SQLite version: %s" % data
 
+    def cleanDatabase(self):
+        with self.conn:
+            self.cur.execute("DELETE FROM TABLE {}".format(self.table_name))
 
+    def bootstrapData(self):
 
-    '''#connectToSocket
-    def connect(self):
+        sample_data = {
+            'num_channels': 8,
+            'channel_locations': ["Fp1", "Fp2", "F3", "F4", "C3", "Cz", "C4", "ref"],
+            'sample_rate': 250,
+            'reference': 8,
+            'data': "1,2,3,4,5,6,7,8"
+        }
+
+        initdata = (
+            ('AlexR','Softies', dt.datetime.today(), json.dumps(sample_data)),
+            ('AlexM','Softies', dt.datetime.today(), json.dumps(sample_data)),
+            ('MikeW','Hardons', dt.datetime.today(), json.dumps(sample_data)),
+            ('MikeL','Hardons', dt.datetime.today(), json.dumps(sample_data)),
+            ('RaulP','Hardons', dt.datetime.today(), json.dumps(sample_data))
+        )
+        with self.conn:
+            self.cur.executemany("INSERT INTO {}(subj_name, group_label, date, data) VALUES(?, ?, ?, ?)".format(self.table_name), initdata)
+                #sendAllData
+    def sendAllData(self):
+        with self.conn:
+            self.cur.execute("SELECT * FROM {}".format(table_name))
+
+            rows = self.cur.fetchall()
+
+            for row in rows:
+                print row
+    '''
 
     #storeLastHourOfData
     def storeLastHourOfData(self):
@@ -68,8 +103,7 @@ class DataStorage:
     #sendLastHourOfData
     def sendLastHourOfData(self):
 
-    #sendAllData
-    def sendAllData(self):
+
 
     #sendAllSegmentsBySubject
     def sendAllSegmentsBySubject(self):
@@ -83,4 +117,6 @@ class DataStorage:
 
 if __name__ == '__main__':
     DB = DataStorage()
+    DB.cleanDatabase()
+    DB.bootstrapData()
 
